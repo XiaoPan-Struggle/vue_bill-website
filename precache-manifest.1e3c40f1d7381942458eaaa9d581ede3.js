9643e63e618a36400b0a4792a9cc9517f3446cb2
XiaoPan <2489964425@qq.com>
2021-09-21 17:46:58 +0800

    deploy

---
+++ precache-manifest.1e3c40f1d7381942458eaaa9d581ede3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479f95f96ba04a639714",
+    "url": "/vue_bill-website/css/app.97341887.css"
+  },
+  {
+    "revision": "dc818eef898bf4a00debc95f357d4d47",
+    "url": "/vue_bill-website/index.html"
+  },
+  {
+    "revision": "479f95f96ba04a639714",
+    "url": "/vue_bill-website/js/app.1a561a85.js"
+  },
+  {
+    "revision": "d6c01cbdb41154338357",
+    "url": "/vue_bill-website/js/chunk-vendors.4c3728d1.js"
+  },
+  {
+    "revision": "c978d6ab2cea1a0036d1696e088dea3b",
+    "url": "/vue_bill-website/manifest.json"
+  },
+  {
+    "revision": "376ed375c3b08a925ade01e78d0ecf26",
+    "url": "/vue_bill-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_bill-website/robots.txt"
+  }
+]);
